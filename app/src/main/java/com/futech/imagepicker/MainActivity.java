6d316b5c724ffa52fe76dbe455c73419674294d6
santosh102 <santoshmadani102@gmail.com>
2021-12-18 22:46:32 -0800

    initial commit

--- app/src/main/java/com/futech/imagepicker/MainActivity.java
+++ app/src/main/java/com/futech/imagepicker/MainActivity.java
@@ -12,10 +12,14 @@ import android.widget.ImageView;
 import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class MainActivity extends AppCompatActivity {
 
     ImageView cover;
+    CircleImageView profile;
     FloatingActionButton actionButton;
+    FloatingActionButton profile1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
         cover=findViewById(R.id.coverImage);
         actionButton=findViewById(R.id.floatingActionButton);
 
+        profile1=findViewById(R.id.changeProfile);
+        profile=findViewById(R.id.profile);
+
         actionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,7 +39,19 @@ public class MainActivity extends AppCompatActivity {
 //                        .crop()	    			//Crop image(Optional), Check Customization for more option
 //                        .compress(1024)			//Final image size will be less than 1 MB(Optional)
 //                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
-                          .start();
+                          .start(10);
+
+            }
+        });
+
+        profile1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImagePicker.with(MainActivity.this)
+//                        .crop()	    			//Crop image(Optional), Check Customization for more option
+//                        .compress(1024)			//Final image size will be less than 1 MB(Optional)
+//                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                        .start(20);
 
             }
         });
@@ -44,7 +63,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+        if(requestCode==10){
         Uri uri=data.getData();
         cover.setImageURI(uri);
+        }
+        if(requestCode==20){
+            Uri uri=data.getData();
+            profile.setImageURI(uri);
+        }
     }
 }
